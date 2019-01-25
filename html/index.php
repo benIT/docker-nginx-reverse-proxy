80fe67aeecf8233046a0a052ed6ebfba9988af43
benIT <benoit.works@gmail.com>
2019-01-25 14:54:59 +0100

    enhance demo

--- html/index.php
+++ html/index.php
@@ -1,20 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <style>
-        body {
-            background-color: <?php echo getenv('color'); ?>;
-            color:white;
-        }
-    </style>
     <meta charset="UTF-8">
     <title><?php echo getenv('color'); ?></title>
+    <style>
+        body {
+            background-color: black;
+        }
+        .circle {
+            width: 250px;
+            height: 250px;
+            border-radius: 125px;
+            font-size: 15px;
+            color: #fff;
+            line-height: 250px;
+            text-align: center;
+            background-color: <?php echo getenv('color');?>;
+        }
+    </style>
 </head>
 <body>
-<h1><?php echo getenv('color'); ?></h1>
+<div class="circle">
+    <span> <?php echo sprintf('dispatched on %s', $_SERVER['SERVER_ADDR']); ?></span>
 
+</div>
 <script>
-    setTimeout(function(){
+    setTimeout(function () {
         window.location.reload(1);
     }, 1000);
 </script>
